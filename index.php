7aed2224d8ca9c174ae7aa2d62ced5420c3be52c
Yago Ananias <yago.crazy@hotmail.com>
2017-09-15 17:58:06 -0300

    Tela de Login

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \yagoananias\Page;
 use \yagoananias\PageAdmin;
+use \yagoananias\Model\User;
 
 $app = new Slim(); //Chama uma nova aplicação do Slim
 
@@ -21,9 +24,39 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST['login'], $_POST['password']);
+
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function() {
+
+	User::Logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
